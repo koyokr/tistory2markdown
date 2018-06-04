906a3132de7a2cdef4a0dc0e7b72c13d4bb9ab87
koyo <koyo@koyo.kr>
2018-06-04 05:22:54 +0000

    Write class

--- tistory2markdown.py
+++ tistory2markdown.py
@@ -7,19 +7,49 @@ import re
 import sys
 import shutil
 from datetime import datetime
-from time import sleep
-from typing import Generator, List, NoReturn, Tuple
+from typing import Generator, List, NamedTuple, NoReturn, Tuple
 from xml.etree import ElementTree
 
-import html2text
+from html2text import HTML2Text
 from bs4 import BeautifulSoup
 from requests import Session
 
 
-Post = Tuple[int, str, Tuple[str], datetime, str]
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
 
 
-def decompose_not_content(article: BeautifulSoup) -> None:
+class Post(NamedTuple):
+    entry: int
+    title: str
+    categories: Tuple[str, ...]
+    date: datetime
+    content: str
+
+
+def lint_codeblock(text: str) -> str:
+    def remove_indent(t):
+        for line in t.splitlines():
+            if line[0:4] == '    ':
+                yield line[4:]
+            elif line[0:1] == '\t':
+                yield line[1:]
+            else:
+                yield line
+    text = re.sub(r'    .+\n(?:    .*?\n)+', '\n```\n\\g<0>```\n', text)
+    text = '\n'.join(remove_indent(text))
+    text = text.replace('\n\n```\n\n', '\n```\n\n')
+    return text
+
+
+def lint_table(text: str) -> str:
+    t = re.sub(r' \n\n\| \n\n',               '|',     text)
+    t = re.sub(r' \n\n\|',                    '|',        t)
+    t = re.sub(r'(.*?(?:\|.*?)+\|?)   ?\n  ', '|\\g<1>|', t)
+    t = re.sub(r'(\|(?:.*?\|)+\n)+',          '\\g<0>\n', t)
+    return text
+
+
+def decompose_not_content(article: BeautifulSoup):
     clss = ('titleWrap', 'container_postbtn', 'relatedWrap', 'author')
     for cls in clss: 
         div = article.find('div', {'class': cls})
@@ -28,127 +58,125 @@ def decompose_not_content(article: BeautifulSoup) -> None:
 
 
 def get_post(html: str) -> Post:
-    soup = BeautifulSoup(html, 'html5lib')
-    article = soup.find('article')
-
+    article = BeautifulSoup(html, 'html5lib').find('article')
     if not article:
         raise ValueError('Not found article element')
+
     info = article.find('div', {'class': 'titleWrap'})
     if not info:
         raise ValueError('Not found div elements with class titleWrap')
 
     a = info.find('a')
-    entry = int(a.get('href').split('/')[-1])
+    entry = get_entry(a.get('href'))
     title = a.get_text()
-    categories_text = info.find('span', {'class': 'category'}).get_text() \
-                          .replace('분류없음', 'uncategorized')
-    categories = tuple(categories_text.strip().split('/'))
+
+    category = info.find('span', {'class': 'category'})
+    category_text = category.get_text().replace('분류없음', 'uncategorized')
+    categories = tuple(category_text.strip().split('/'))
+
     date_text = info.find('span', {'class': 'date'}).get_text()
     date = datetime.strptime(date_text.strip(), '%Y.%m.%d %H:%M')
 
-    def indent_to_backquote(text):
-        def remove_indent(t):
-            for line in t.splitlines():
-                if line[0:4] == '    ':
-                    yield line[4:]
-                elif line[0:1] == '\t':
-                    yield line[1:]
-                else:
-                    yield line
-        p = re.compile(r'    .+\n(?:    .*?\n)+')
-        text_backquote = p.sub('\n```\n\\g<0>```\n', text)
-        return '\n'.join(remove_indent(text_backquote))
-
     decompose_not_content(article)
-    h = html2text.HTML2Text(bodywidth=0)
-    content = indent_to_backquote(h.handle(article.prettify()))
-    return entry, title, categories, date, content
+    content = HTML2Text(bodywidth=0).handle(article.prettify())
+    content = lint_codeblock(content)
+    content = lint_table(content)
+    return Post(entry, title, categories, date, content)
 
 
-def get_entry_last(sess: Session, base: str) -> int:
-    rlink = f'{base}/rss'
-    with sess.get(rlink) as resp:
-        rss = ElementTree.fromstring(resp.text)
-        plink_last = rss.find('channel').find('item').find('link').text
-        entry_last = int(plink_last.split('/')[-1])
-    return entry_last
+def get_entry(url: str) -> int:
+    path = url.split('/')[-1]
+    return int(path)
 
 
-def iter_post(sess: Session, base: str) -> Generator[Post, None, None]:
-    entry_last = get_entry_last(sess, base)
-    for entry in range(1, entry_last + 1):
-        plink = f'{base}/{entry}'
-        with sess.get(plink) as resp:
-            try:
-                yield get_post(resp.text)
-            except ValueError as e:
-                print(f'[!] {e}', file=sys.stderr)
+class Tistory():
+    __slot__ = ('sess', 'host', 'link')
+
+    def __init__(self, host: str):
+        self.sess: Session = Session()
+        self.host: str = host
+        self.link: str = f'http://{host}'
+        self.sess.headers.update({'user-agent': USER_AGENT})
+
+    def __del__(self):
+        self.sess.close()
+
+    def save_post_all(self):
+        for post in self.iter_post():
+            self.save_post(post)
+
+    def iter_post(self) -> Generator[Post, None, None]:
+        for entry in range(1, self.get_entry_last() + 1):
+            with self.sess.get(f'{self.link}/{entry}') as resp:
+                try:
+                    yield get_post(resp.text)
+                except ValueError as e:
+                    print(f'[!] {e}', file=sys.stderr)
+
+    def get_entry_last(self) -> int:
+        with self.sess.get(f'{self.link}/rss') as resp:
+            rss = ElementTree.fromstring(resp.text)
+        url_post_last = rss.find('channel').find('item').find('link').text
+        entry_last = get_entry(url_post_last)
+        return entry_last
+
+    def fix_content_save_img(self, dirpath: str, content: str) -> str:
+        index = 0
+        def get_img_path_download(m) -> str:
+            nonlocal index
+            index += 1
+            imgurl = m.group(1)
+            imgpath_temp = f'{dirpath}/img_{index}'
+            with self.sess.get(imgurl, stream=True) as resp:
+                with open(imgpath_temp, 'wb') as f:
+                    f.write(resp.content)
+            ext = imghdr.what(imgpath_temp)
+            imgpath = f'{imgpath_temp}.{ext}'
+            shutil.move(imgpath_temp, imgpath)
+            print(imgpath)
+            imgname = os.path.basename(imgpath)
+            return f'![{imgname}]({imgname})'
+        return re.sub(r'!\[\]\((.*?)\)', get_img_path_download, content)
+
+    def save_post(self, post: Post):
+        entry, title, categories, date, content = post
+        categories_path = '/'.join(categories)
+        categories_text = ', '.join(categories)
+        dirpath = f'{self.host}/{categories_path}/{entry}'
+        os.makedirs(dirpath)
+
+        content = self.fix_content_save_img(dirpath, content)
+        title = f'"{title}"' if ':' in title else title
+
+        buf = f'---\n' \
+              f'title: {title}\n' \
+              f'categories: [{categories_text}]\n' \
+              f'slug: {entry}\n' \
+              f'date: {date}\n' \
+              f'url: {entry}\n' \
+              f'---\n' \
+              f'{content}'
+        
+        mdpath = f'{dirpath}/index.md'
+        with open(mdpath, 'w') as f:
+            f.write(buf)
+        print(mdpath)
 
 
-def main(argv: Tuple[str]) -> None:
+def main(argv: Tuple[str]):
     if len(argv) != 2:
         help()
-
-    scheme = 'http'
     host = argv[1]
-    base = f'{scheme}://{host}'
-    ua = 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
-    mdname = 'index.md'
-    re_img = re.compile(r'!\[\]\(' \
-                        r'(https://t1\.daumcdn\.net/cfile/tistory/\w+)' \
-                        r'\)')
 
-    try:
-        os.makedirs(host)
-    except FileExistsError as e:
-        if input(f'[!] {e.args[1]}: Remove "{host}"? (y/n):').lower() == 'y':
+    if os.path.exists(host):
+        if input(f'[!] File exists: Remove "{host}"? (y/N):').lower() == 'y':
             shutil.rmtree(host)
-            os.makedirs(host)
         else:
             return
+    os.makedirs(host)
 
-    with Session() as sess:
-        sess.headers.update({'user-agent': ua})
-        for entry, title, categories, date, content in iter_post(sess, base):
-            category = categories[0]
-            categories_path = '/'.join(categories)
-            categories_text = ', '.join(categories)
-
-            dirpath = f'{host}/{categories_path}/{entry}'
-            mdpath = f'{dirpath}/{mdname}'
-            os.makedirs(dirpath)
-
-            index = 0
-            def get_img_path_save(m) -> str:
-                nonlocal index
-                index += 1
-                ilink = m.group(1)
-                ipath = f'{dirpath}/img_{index}'
-                with sess.get(ilink, stream=True) as resp:
-                    with open(ipath, 'wb') as f:
-                        f.write(resp.content)
-                ext = imghdr.what(ipath)
-                ipath_ext = f'{ipath}.{ext}'
-                shutil.move(ipath, ipath_ext)
-                print(ipath_ext)
-                iname = f'img_{index}.{ext}'
-                return f'![{iname}]({iname})'
-            content_with_img = re_img.sub(get_img_path_save, content)
-            title = f'"{title}"' if ':' in title else title
-
-            buf = f'---\n' \
-                  f'title: {title}\n' \
-                  f'categories: [{categories_text}]\n' \
-                  f'slug: {entry}\n' \
-                  f'date: {date}\n' \
-                  f'---\n' \
-                  f'{content_with_img}'
-            
-            with open(mdpath, 'w') as f:
-                f.write(buf)
-            print(mdpath)
-            sleep(0.1)
-
+    tistory = Tistory(host)
+    tistory.save_post_all()
 
 
 def help() -> NoReturn:
